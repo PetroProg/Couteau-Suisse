c111fb6c787109e7d7d3370ed143532bb3a43da5
Petro Maltsev <ph62pse@eduvaud.ch>
2026-09-11 10:56:26 +0200

    second feat: transform from decimal to binary in 2 ways

--- morse/morse.cs
+++ morse/morse.cs
@@ -8,39 +8,109 @@ namespace Morse
         static void Main(string[] args)
         {
             // Déclaration des variables
-            char chrChoix = ' ';  // Variable pour stocker le choix de l'utilisateur
+            char chrChoix = ' ';  // Variable pour stocker les choix de l'utilisateur
+            
             string strInput = ""; // Variable pour stocker le texte saisi par l'utilisateur
             string strMorseCode = "";  // Variable pour stocker le code Morse correspondant
+            string strBinary = ""; // Variable pour stocker le nombre binaire saisi par l'utilisateur
+            string strBinaryInput = ""; // Variable pour stocker le nombre binaire saisi par l'utilisateur
+            
+            uint uintDecimal = 0; // Variable pour stocker le nombre décimal saisi par l'utilisateur
+            uint uintBinaireToDecimal = 0; // Variable pour stocker le nombre décimal converti à partir du binaire
+
             
             // Boucle principale pour permettre à l'utilisateur de convertir plusieurs textes
             do{
-                Console.WriteLine("=== Convertisseur du texte en Morse ===");
-                Console.Write("Entrez un mot ou une phrase (sans accents, lettres A-Z) : ");
+                Console.Clear(); // Efface la console pour une meilleure lisibilité
 
-                // Lecture de l'entrée utilisateur
-                strInput = Console.ReadLine() ?? string.Empty;
+                // Affichage du menu principal
+                Console.WriteLine("=== Couteau Suisse - Utilitaires ===");
+                Console.WriteLine("1. Convertir du texte en code Morse");
+                Console.WriteLine("2. Convertir des nombres entre différentes bases (Décimal <> Binaire <> Octal)");
+                Console.WriteLine("3. En production");
+                Console.Write("Veuillez entrer votre choix : ");
 
-                // Remplace les espaces entre les mots par un séparateur visible
-                strInput = VerifySpaces(strInput);
-
-                // Conversion du texte en code Morse via une méthode dédiée
-                strMorseCode = ConvertToMorse(strInput);
-
-                // Affichage du code Morse correspondant
-                Console.WriteLine("Le code Morse correspondant est : " + strMorseCode);
+                // Lecture du choix de l'utilisateur
+                chrChoix = Console.ReadKey().KeyChar;
                 Console.WriteLine();
+                Console.WriteLine("==========================================================");
+                // Traitement du choix de l'utilisateur
+                switch (chrChoix)
+                {
+                    case '1':
+                            Console.Write("Entrez un mot ou une phrase (sans accents, lettres A-Z) : ");
 
-                // Option pour sauvegarder le code Morse dans un fichier
-                SaveResponseToFile(strMorseCode);
+                            // Lecture de l'entrée utilisateur
+                            strInput = Console.ReadLine() ?? string.Empty;
+
+                            // Remplace les espaces entre les mots par un séparateur visible
+                            strInput = VerifySpaces(strInput);
+
+                            // Conversion du texte en code Morse via une méthode dédiée
+                            strMorseCode = ConvertToMorse(strInput);
+
+                            // Affichage du code Morse correspondant
+                            Console.WriteLine("Le code Morse correspondant est : " + strMorseCode);
+                            Console.WriteLine();
+
+                            // Option pour sauvegarder le code Morse dans un fichier
+                            SaveResponseToFile(strMorseCode);
+                        break;
+                    case '2':
+                            Console.WriteLine("1. Décimal > Binaire");
+                            Console.WriteLine("2. Binaire > Décimal");
+                            Console.WriteLine("3. Binaire > Octal");
+                            Console.WriteLine("4. Octal > Binaire");
+                            Console.Write("Veuillez entrer votre choix : ");
+                            chrChoix = Console.ReadKey().KeyChar;
+                            Console.WriteLine();
+                                switch (chrChoix)
+                                {
+                                    case '1':
+                                        Console.Write("Entrez un nombre décimal : ");
+                                        uintDecimal = Convert.ToUInt32(Console.ReadLine());
+                                        strBinary = ConvertDecimalToBinary(uintDecimal);
+                                        Console.WriteLine("Le nombre binaire correspondant est : " + strBinary);
+                                        break;
+                                    case '2':
+                                        Console.Write("Entrez un nombre binaire : ");
+                                        strBinaryInput = Console.ReadLine();
+                                        uintBinaireToDecimal = ConvertBinaryToDecimal(strBinaryInput);
+                                        Console.WriteLine("Le nombre décimal correspondant est : " + uintBinaireToDecimal);
+                                        break;
+                                    case '3':
+                                        Console.Write("Entrez un nombre binaire : ");
+                                        break;
+                                    case '4':
+                                        Console.Write("Entrez un nombre octal : ");
+                                        break;
+                                    default:
+                                        Console.WriteLine("Choix invalide.");
+                                        break;
+                                }
+                            break;
+                    case '3':
+                        Console.WriteLine("En production");
+                        break;
+
+                    default:
+                        Console.WriteLine("Choix invalide.");
+                        break;
+                }
+
+                
+
+               
                 
                 // Demande à l'utilisateur s'il souhaite convertir un autre texte
-                Console.Write("Voulez-vous convertir un autre texte ? (O/N) : ");
+                Console.Write("Voulez-vous continuer ? (O/N) : ");
                 chrChoix = Console.ReadKey().KeyChar;
                 Console.WriteLine();
             }
             while (chrChoix == 'O' || chrChoix   == 'o');
         }  
-        
+
+        // Méthode pour vérifier et traiter les espaces dans l'entrée utilisateur
         static string VerifySpaces (string strInput)
         {
             // Vérifie si l'entrée utilisateur contient des espaces
@@ -94,11 +164,12 @@ namespace Morse
             return strReponse;
         }   
 
+        // Méthode pour sauvegarder le code Morse dans un fichier
         static void SaveResponseToFile(string strMorseCode)
         {
-            string strFilePath = @"E:\Modules\I114\Projet\reponseMorse.txt"; // Définition du chemin du fichier de sortie
+            string strFilePath = @"E:\Modules\I114\Projet\Codification-du-Morse\reponseMorse.txt"; // Définition du chemin du fichier de sortie
 
-            Console.WriteLine("Voulez-vous sauvegarder le code Morse dans un fichier ? (O/N) : ");
+            Console.Write("Voulez-vous sauvegarder le code Morse dans un fichier ? (O/N) : ");
             char chrChoix = Console.ReadKey().KeyChar;
             Console.WriteLine(); // Pour passer à la ligne suivante
 
@@ -112,7 +183,61 @@ namespace Morse
                 // Si le fichier existe déjà, on ajoute le code Morse à la fin du fichier existant
                 File.AppendAllText(strFilePath, strMorseCode + Environment.NewLine);
             }
-        }  
+        }
+
+        // Méthode pour convertir un nombre décimal en binaire
+        static string ConvertDecimalToBinary(uint uintDecimal)
+        {
+            if (uintDecimal == 0) return "0";
+
+            string strResultatBinaire = "";
+            bool boolTrouverUn = false;
+
+            // Parcours des bits de 31 à 0 pour construire la représentation binaire
+            for (int i = 31; i >= 0; i--)
+            {
+                uint uintPuissance = (uint)Math.Pow(2, i); 
+                uint uintBit = uintDecimal / uintPuissance;
+
+                if (uintBit == 1)
+                {
+                    boolTrouverUn = true;
+                }
+
+                if (boolTrouverUn)
+                {
+                    strResultatBinaire += uintBit;
+                    uintDecimal %= uintPuissance; 
+                }
+            }
+
+            return strResultatBinaire;
+        }
+
+        static uint ConvertBinaryToDecimal(string strBinaryInput)
+        {
+            uint uintDecimalResult = 0;
+            byte bytLength = (byte)strBinaryInput.Length;
+            
+            for (int i = 0; i < bytLength; i++)
+            {
+                if (strBinaryInput[bytLength - 1 - i] == '1')
+                {
+                    uintDecimalResult += (uint)Math.Pow(2, i);
+                }
+                else if (strBinaryInput[bytLength - 1 - i] == '0')
+                {
+                    // Ne rien faire, le bit est déjà à 0
+                }
+                else
+                {
+                    Console.WriteLine("Erreur : Le nombre binaire contient un caractère non valide.");
+                    return 0;
+                }
+            }
+
+            return uintDecimalResult;
+        }
     }
 }
 
